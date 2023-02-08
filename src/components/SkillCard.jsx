6643fd2d65e7d247e0078ff9f801d314d0b36658
Olivia Schwartz <ocmooney@gmail.com>
2023-02-08 09:55:30 -0500

    updated skills

---
+++ src/components/SkillCard.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+const SkillCard = ({icon, name}) => {
+    return (
+        <div className='skill-card'>
+            {icon}
+            <h3>{name}</h3>
+        </div>
+    )
+}
+
+export default SkillCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import '../App.css'
 import {Link} from 'react-router-dom'
+import {  DiCss3Full, DiHtml5, DiMongodb, DiNodejsSmall, DiReact  } from "react-icons/di";
+import { SiExpress, SiJavascript, SiFlask, SiPostgresql, SiSequelize, SiTypescript } from 'react-icons/si'
+import { FaVuejs, FaPython } from 'react-icons/fa'
+import SkillCard from '../components/SkillCard';
+
 
 function Home () {
     return(
@@ -18,6 +23,25 @@ function Home () {
                 <Link to='/portfolio'><button className="explore-button"><h3>EXPLORE WORK >>> </h3></button></Link>
             </div>
             </header>
+            <div className="skills">
+                <h1 className="skillsHeader">Skills</h1>
+            <div className='skills-icon-container'>
+                <SkillCard icon={<DiHtml5 className='icon'/>} name="HTML" />
+                <SkillCard icon={<DiCss3Full className='icon'/>} name="CSS" />
+                <SkillCard icon={<SiJavascript className='icon'/>} name="JavaScript" />
+                <SkillCard icon={<DiReact className='icon'/>} name="React.js" />
+                <SkillCard icon={<FaVuejs className='icon'/>} name="Vue.js" />
+                <SkillCard icon={<SiExpress className='icon'/>} name="Express.js" />
+                <SkillCard icon={<DiNodejsSmall className='icon'/>} name="Node.js" />
+                <SkillCard icon={<FaPython className='icon'/>} name="Python" />
+                <SkillCard icon={<SiFlask className='icon'/>} name="Flask" />
+                <SkillCard icon={<DiMongodb className='icon'/>} name="MongoDB" />
+                <SkillCard icon={<SiPostgresql className='icon'/>} name="PostgreSQL" />
+                <SkillCard icon={<SiSequelize className='icon'/>} name="Sequelize" />
+                <SkillCard icon={<SiTypescript className='icon'/>} name="TypeScript" />
+            </div>
+            </div>
+
         </div>
     )
 }
